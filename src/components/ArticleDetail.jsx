88efecefe73233fe676fbd1fc0a67afcba9e455c
davidchunghc <davidchunghc@gmail.com>
2024-06-18 15:36:46 +0100

    Core 5 done - View list of comments associated with article

--- src/components/ArticleDetail.jsx
+++ src/components/ArticleDetail.jsx
@@ -1,23 +1,40 @@
 import { useEffect, useState } from "react";
-import { fetchArticleById } from "../utils/api.js";
+import { fetchArticleById, fetchCommentsByArticleId } from "../utils/api.js";
 import { useParams } from "react-router-dom";
 import "../App.css";
+import CommentCard from "./CommentCard.jsx";
 
 const ArticleDetail = () => {
   const { articleId } = useParams();
   const [article, setArticle] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [comments, setComments] = useState([]);
   const [error, setError] = useState("");
 
   useEffect(() => {
     fetchArticleById(articleId)
       .then((articleFromApi) => {
         setArticle(articleFromApi);
+        setIsLoading(false);
       })
       .catch((error) => {
         setError("Fetch article details failed");
         throw error;
       });
+
+    fetchCommentsByArticleId(articleId)
+      .then((commentsFromApi) => {
+        setComments(commentsFromApi);
+      })
+      .catch((error) => {
+        setError("Fetch comments failed");
+        throw error;
+      });
   }, [articleId]);
+
+  if (isLoading) {
+    return <p>Loading Article...</p>;
+  }
 
   if (error) return <p>{error}</p>;
 
@@ -28,19 +45,24 @@ const ArticleDetail = () => {
 
   return (
     <section>
-      <li>
-        <h2>Topic: {article.topic}</h2>
-        <h3>Title: {article.title}</h3>
-        <h4>Author: {article.author}</h4>
-        <img src={article.article_img_url} />
-        <p>Body: {article.body}</p>
-        <p>Created at: {formatDate(article.created_at)}</p>
-        <p>Votes: {article.votes}</p>
-        <div>
-          <button className="voteButton">+</button>
-          <button className="voteButton">-</button>
-        </div>
-      </li>
+      <h2>Topic: {article.topic}</h2>
+      <h3>Title: {article.title}</h3>
+      <h4>Author: {article.author}</h4>
+      <img src={article.article_img_url} />
+      <p>Body: {article.body}</p>
+      <p>Created at: {formatDate(article.created_at)}</p>
+      <p>Votes: {article.votes}</p>
+      <div>
+        <button className="vote-button">+</button>
+        <button className="vote-button">-</button>
+      </div>
+
+      <h3>Comments</h3>
+      <div>
+        {comments.map((comment) => (
+          <CommentCard key={comment.comment_id} comment={comment} />
+        ))}
+      </div>
     </section>
   );
 };
