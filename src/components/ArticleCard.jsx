e7687866713b92ee3597f8a5206ed3a5abb9a743
davidchunghc <davidchunghc@gmail.com>
2024-06-18 09:40:48 +0100

    try to merge core 2 first

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -7,6 +7,7 @@ const ArticleCard = ({ article }) => {
         <h3>Topic: {article.topic}</h3>
         <h4>Title: {article.title}</h4>
         <h5>Author: {article.author}</h5>
+        <p>Body: {article.body}</p>
         <p>Votes: {article.votes}</p>
       </li>
     </section>
